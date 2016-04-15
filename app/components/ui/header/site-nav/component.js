e28d929bae8294a136f102845162e0e66aae03d3
MrDinsdale <joe@bolser.co.uk>
2016-04-15 09:13:48 +0100

    Design update WIP

--- app/components/ui/header/site-nav/component.js
+++ app/components/ui/header/site-nav/component.js
@@ -1,4 +1,41 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'nav',
+  classNames: ['site-navigation'],
+  classNameBindings: ['navState:clicked:unclicked'],
+  timeline: [],
+
+  navStateMap: {
+    0: 'navCloseAnimation',
+    1: 'navOpenAnimation'
+  },
+
+  navCloseAnimation: function() {
+    Ember.$('.nav-button__toggle').removeClass("clicked");
+    this.timeline.reverse();
+  },
+
+  navOpenAnimation: function() {
+    Ember.$('.nav-button__toggle').addClass("clicked");
+    this.timeline.play();
+  },
+
+  navAnimation: (function() {
+    this.timeline = new TimelineLite({ paused: true });
+
+    this.timeline.append(TweenMax.to( Ember.$('#nav-left'), 0.3, {
+      left: "0vw",
+      ease: Power2.easeInOut
+    }), 0);
+
+    this.timeline.append(TweenMax.to( Ember.$('#nav-right'), 0.3, {
+      right: "0vw",
+      ease: Power2.easeInOut
+    }), -0.3);
+  }).on('didInsertElement'),
+
+  navStateChanged: (function() {
+    return this[this.get('navStateMap')[this.get('navState')]]();
+  }).observes('navState')
 });
