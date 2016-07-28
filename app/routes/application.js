8a9423b4b9e14de6908f7778679857ae7c9ef50f
Joe Dinsdale <mrjdinsdale@gmail.com>
2016-07-28 22:44:58 +0100

    Revert "Added dynamic meta description"
    This reverts commit 6ff913ab774d9888b7ed531868770ffcae7d8170.

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,41 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   title: function(tokens) {
     return 'Joe Dinsdale' + ' | ' + tokens.join();
-  },
-
-  headTags: function() {
-    let description = 'I am a frontend web developer based in the Leeds area who loves HTML, Sass, Ember and React.';
-    return [{
-      type: 'meta',
-      tagId: 'meta-description-tag',
-      attrs: {
-        name: 'description',
-        content: description
-      }
-    },
-    {
-      type: 'meta',
-      tagId: 'twitter-description-tag',
-      attrs: {
-        name: 'twitter:description',
-        content: description
-      }
-    },
-    {
-      type: 'meta',
-      tagId: 'og-description-tag',
-      attrs: {
-        name: 'og:description',
-        content: description
-      }
-    },
-    {
-      type: 'meta',
-      tagId: 'schema-description-tag',
-      attrs: {
-        itemprop: 'description',
-        content: description
-      }
-    }];
   }
 });
