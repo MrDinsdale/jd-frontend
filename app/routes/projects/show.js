85f59cfd090dd6aa261ea82d3d0530ef0f670325
Joe Dinsdale <mrjdinsdale@gmail.com>
2016-07-29 19:54:49 +0100

    Revert "Revert "Added dynamic meta description""
    This reverts commit 8a9423b4b9e14de6908f7778679857ae7c9ef50f.
    
    # Conflicts:
    #	app/index.html

--- app/routes/projects/show.js
+++ app/routes/projects/show.js
@@ -11,5 +11,42 @@ export default Ember.Route.extend({
     controller.set('project', project);
 
     this.set('titleToken', project.get('title'));
+
+    let description = project.get('excerpt');
+
+    let pageMeta = [{
+      type: 'meta',
+      tagId: 'meta-description-tag',
+      attrs: {
+        name: 'description',
+        content: description
+      }
+    },
+    {
+      type: 'meta',
+      tagId: 'twitter-description-tag',
+      attrs: {
+        name: 'twitter:description',
+        content: description
+      }
+    },
+    {
+      type: 'meta',
+      tagId: 'og-description-tag',
+      attrs: {
+        name: 'og:description',
+        content: description
+      }
+    },
+    {
+      type: 'meta',
+      tagId: 'schema-description-tag',
+      attrs: {
+        itemprop: 'description',
+        content: description
+      }
+    }];
+
+    this.set('headTags', pageMeta);
   }
 });
