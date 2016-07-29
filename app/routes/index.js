f6b30add893a5d2b4a7bbd721bad98533a7cc791
Joe Dinsdale <mrjdinsdale@gmail.com>
2016-07-29 08:48:20 +0100

    Cleaned up some junk

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,14 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   titleToken: 'Home',
-
-  // model: function() {
-  //   return this.store.findAll('post');
-  // },
-
-  // setupController: function(controller, model) {
-  //   var posts = model;
-
-  //   controller.set('posts', posts);
-  // }
 });
